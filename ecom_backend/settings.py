f59aba7ce2db7e4f2f84c7fc47ca8792b9153d66
henry <aengrhenry@gmail.com>
2022-03-22 01:13:11 +0100

    implement cors header feature to allow only our frontend client to access our api

--- ecom_backend/settings.py
+++ ecom_backend/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    "corsheaders",
     'rest_framework',
     'account',
     'products',
@@ -34,11 +34,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+CORS_ALLOWED_ORIGINS=[
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'ecom_backend.urls'
